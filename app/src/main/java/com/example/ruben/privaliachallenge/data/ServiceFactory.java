24c3284b57e0cd88b3b6551f883f47c3005416c3
Ruben <viciana.ruben@gmail.com>
2018-05-23 13:34:12 +0200

    Created Retrofit ServiceFactory

--- app/src/main/java/com/example/ruben/privaliachallenge/data/ServiceFactory.java
+++ app/src/main/java/com/example/ruben/privaliachallenge/data/ServiceFactory.java
@@ -1,4 +1,48 @@
 package com.example.ruben.privaliachallenge.data;
 
+import com.example.ruben.privaliachallenge.BuildConfig;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static okhttp3.logging.HttpLoggingInterceptor.Level.BODY;
+import static okhttp3.logging.HttpLoggingInterceptor.Level.NONE;
+
 public class ServiceFactory {
+
+    /**
+     * Creates a retrofit service
+     *
+     * @param clazz Java interface of the retrofit service
+     */
+
+    public static <T> T createRetrofitService(final Class<T> clazz, String baseUrl) {
+        final Retrofit retrofit = new Retrofit.Builder().baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .client(createBaseClient())
+                .build();
+
+        return retrofit.create(clazz);
+    }
+
+    private static OkHttpClient createBaseClient() {
+        OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        HttpLoggingInterceptor.Level loggingLevel = BuildConfig.DEBUG ? BODY : NONE;
+        loggingInterceptor.setLevel(loggingLevel);
+        httpClientBuilder.addInterceptor(loggingInterceptor);
+
+        // Time Out up to 30 seconds
+        httpClientBuilder.connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS);
+        httpClientBuilder.addInterceptor(chain -> chain.proceed(chain.request()));
+        return httpClientBuilder.build();
+    }
 }
