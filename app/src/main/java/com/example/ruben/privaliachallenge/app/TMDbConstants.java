566161c155c0c1f6768eb5b286015f3883caedf1
Ruben <viciana.ruben@gmail.com>
2018-05-24 13:07:13 +0200

    Base url constant

--- app/src/main/java/com/example/ruben/privaliachallenge/app/TMDbConstants.java
+++ app/src/main/java/com/example/ruben/privaliachallenge/app/TMDbConstants.java
@@ -2,5 +2,5 @@ package com.example.ruben.privaliachallenge.app;
 
 public class TMDbConstants {
 
-    public static final String BASE_URL = "";
+    public static final String BASE_URL = "https://api.themoviedb.org/3/";
 }
